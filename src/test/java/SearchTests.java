2b9108a06e8543d6c6effc7d28da715ca6abb6d8
Oleg Dunyushkin <o.dunyushkin@gmail.com>
2026-02-17 13:50:07 +0300

    Update SearchTests.java
    Izmenil v editor chtob bil konflikt

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SearchTests {
+public class 321SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
