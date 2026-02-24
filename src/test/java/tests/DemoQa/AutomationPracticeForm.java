984253f2b4cfa709952588e12e54b5d40bac656f
Oleg <o.dunyushkin@gmail.com>
2026-02-24 23:55:55 +0300

    poproboval metodi befor all each i td

--- src/test/java/tests/DemoQa/AutomationPracticeForm.java
+++ src/test/java/tests/DemoQa/AutomationPracticeForm.java
@@ -1,8 +1,28 @@
 package tests.DemoQa;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class AutomationPracticeForm {
+
+    @BeforeAll
+    static void firstConfig() {
+        System.out.println("Zaveli obschi config");
+    }
+
+    @AfterAll
+    static void end () {
+        System.out.println("Vse!");
+
+    }
+    @BeforeEach
+    void startBrowser () {
+        System.out.println("Browser Poehal");
+    }
+
+    @AfterEach
+    void closeBrowser () {
+        System.out.println("Zakrili browser");
+    }
 
     @Test
     void firstTest() {
