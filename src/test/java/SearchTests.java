14380c58453780471b7434b01d80dd1057a83fea
Oleg <o.dunyushkin@gmail.com>
2026-02-24 22:22:49 +0300

    DemoQA forma

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchTests {
 
     @Test
-    void 321successfulSearchTest() {
+    void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
